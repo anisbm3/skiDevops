962f9b4d62736f496be355e24035bcb8dd3fefe5
emna44 <emna.borgi@esprit.tn>
2025-04-12 23:28:27 +0100

    first commit

---
+++ src/main/java/tn/esprit/spring/services/PisteServicesImpl.java
@@ -0,0 +1,41 @@
+package tn.esprit.spring.services;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import tn.esprit.spring.entities.Piste;
+import tn.esprit.spring.repositories.IPisteRepository;
+
+import java.util.List;
+@Slf4j
+@AllArgsConstructor
+@Service
+public class PisteServicesImpl implements  IPisteServices{
+
+    public void executer() {
+        log.info("Exécution du service avec Lombok");
+    }
+
+
+    private IPisteRepository pisteRepository;
+
+    @Override
+    public List<Piste> retrieveAllPistes() {
+        return pisteRepository.findAll();
+    }
+
+    @Override
+    public Piste addPiste(Piste piste) {
+        return pisteRepository.save(piste);
+    }
+
+    @Override
+    public void removePiste(Long numPiste) {
+        pisteRepository.deleteById(numPiste);
+    }
+
+    @Override
+    public Piste retrievePiste(Long numPiste) {
+        return pisteRepository.findById(numPiste).orElse(null);
+    }
+}
